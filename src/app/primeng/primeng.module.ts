bf8ffcd8d4330fe280ed11e569abb3f35f44d6d6
mhaniac <portillocastilloa@gmail.com>
2022-08-01 22:27:59 -0600

    Working on header

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
-  exports: [PanelMenuModule, MenuModule],
+  exports: [PanelMenuModule, MenuModule, ButtonModule, SplitButtonModule],
 })
 export class PrimengModule {}
